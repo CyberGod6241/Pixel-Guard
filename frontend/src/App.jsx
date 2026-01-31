dab8daf7e70c4e7458134584754fac8bc7c89e67
Naheeem Abdulazeez <cyberbottv404@gmail.com>
2026-01-31 05:36:56 +0100

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/users");
+      const data = await response.data;
+      console.log("Fetched users:", data.users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <>
@@ -29,7 +42,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/main.py
+++ server/main.py
@@ -1,1 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
 
+app = Flask(__name__)
+cors = CORS(app, origins="*")
+ # This allows your React app to talk to this API
+
+@app.route('/api/users', methods=['GET'])
+def users():
+   return jsonify({
+      "users": [
+         "alice",
+         "zach",
+         "jessie"]
+   })
+
+if __name__ == '__main__':
+   app.run(debug=True, port=8080)
